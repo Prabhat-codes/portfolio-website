1b69ef646d054b75573a7b867559447f16caff6f
Prabhat <prabhat.namdharani@gmail.com>
2024-05-12 20:08:42 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6344e6a674067edb549df813fedad93b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a88fc90c6e9b9e73a66",
+    "url": "/portfolio-website/static/css/main.9c025f42.chunk.css"
+  },
+  {
+    "revision": "7a88fc90c6e9b9e73a66",
+    "url": "/portfolio-website/static/js/main.7a88fc90.chunk.js"
+  },
+  {
+    "revision": "831e629fc4cba0581de0",
+    "url": "/portfolio-website/static/css/1.4b0c3350.chunk.css"
+  },
+  {
+    "revision": "831e629fc4cba0581de0",
+    "url": "/portfolio-website/static/js/1.831e629f.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "1d10b2335bb8bf466ee8b97e0e82a8ce",
+    "url": "/portfolio-website/index.html"
+  }
+];
